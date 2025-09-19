2de9e616a193e1c99d5f5c1d7394ffddb651608c
GiaMat90 <giacominomatzeu@gmail.com>
2025-09-20 01:56:02 +0200

    First commit of my leak detector

---
+++ test/main.cpp
@@ -0,0 +1,35 @@
+#include <tsg/io.h>
+#include <tsld.h>
+#include <array>
+
+template<typename T, std::size_t S, typename ...Args>
+void no_leaks(Args... args) {
+	std::array<T*, S> array;
+	for (std::size_t i{}; i < S; ++i) {
+		array[i] = new T(args...);
+	}
+	for (std::size_t i{}; i < S; ++i) {
+		delete array[i];
+	}
+}
+template<typename T, std::size_t S, typename ...Args >
+void leaks(Args... args) {
+	std::array<T*, 2*S> array;
+	for (std::size_t i{}; i < 2 * S; ++i) {
+		array[i] = new T(args...);
+	}
+	for (std::size_t i{}; i < 2 * S; ++i) {
+		if (i % 2 > 0) { 
+			delete array[i]; 
+		}
+	}
+}
+int main() {
+	tsg::print("Hello, World!\n");
+	
+	no_leaks<int, 100>(42);
+	leaks<int, 100>(42);
+	auto p = new(__FILE__, __LINE__) int(42);
+
+	return 0;
+}
